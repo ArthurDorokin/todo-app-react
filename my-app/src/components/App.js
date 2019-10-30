3b30ca0b93f0a81043835180b6ba4b78dc63a368
Артур <a.u.dorokhin@gmail.com>
2019-10-30 23:38:54 +0200

    finish app

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,18 +1,46 @@
 import React, {Component} from 'react';
+
 import Notes from './Notes';
+import {withoutIndex} from '../utils';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             notes: [
-                "массив"
+                "массив",
+                "массив1",
+                "массив2"
             ]
         };
     }
-    render() {
-        return <Notes notes={this.state.notes}/>;
+
+
+    onNoteDelete = indexToRemove => {
+        this.setState(oldState => {
+            return {
+                notes: withoutIndex(oldState.notes, indexToRemove)
+            };
+        });
     };
+
+    onNoteCreate = newNoteText => {
+        this.setState(oldState => {
+            return {
+                notes: [newNoteText].concat(oldState.notes)
+            };
+        });
+    };
+
+    render() {
+        return (
+            <Notes
+                notes={this.state.notes}
+                onDelete={this.onNoteDelete}
+                onCreate={this.onNoteCreate}
+            />
+        );
+    }
 }
 
 export default App;
